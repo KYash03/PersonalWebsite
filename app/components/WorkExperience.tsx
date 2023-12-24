af74a9f001ce777352f7cad5ee23355b19e83c6b
YASH KUMAR <kyash03@student.ubc.ca>
2023-12-23 23:42:30 -0800

    added work experience section

---
+++ app/components/WorkExperience.tsx
@@ -0,0 +1,3 @@
+export default function WorkExperience() {
+  return <div className="border-l-2 border-gray-200 pl-4 mb-6">TEST</div>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Connect from "./components/Connect";
 import Header from "./components/Header";
 import Intro from "./components/Intro";
 import Education from "./components/Education";
+import WorkExperience from "./components/WorkExperience"; // Make sure you have this component
 
 interface Click {
   clickedState: boolean;
@@ -15,6 +16,7 @@ interface Click {
 export default function Home() {
   const [isClickedConnect, setClickConnect] = useState(true);
   const [isClickedEducation, setClickEducation] = useState(false);
+  const [isClickedWorkExperience, setClickWorkExperience] = useState(false);
 
   const handleClick =
     ({ setClickedState, clickedState }: Click) =>
@@ -28,45 +30,60 @@ export default function Home() {
       <div className="flex h-screen font-sans text-lg">
         <div className="w-full max-w-screen-md flex flex-col space-y-4 mx-auto">
           <Intro />
-          <div>
-            {/* CONNECT */}
-            <button
-              onClick={handleClick({
-                setClickedState: setClickConnect,
-                clickedState: isClickedConnect,
-              })}
-              className="mb-6 text-2xl font-semibold hover:underline flex items-center"
-            >
-              <Image
-                src={isClickedConnect ? "/down.png" : "/right.png"}
-                alt="Toggle"
-                width={20}
-                height={20}
-                className="mr-2"
-              />
-              Connect
-            </button>
-            {isClickedConnect && <Connect />}
-
-            {/* EDUCATION AND AWARDS*/}
-            <button
-              onClick={handleClick({
-                setClickedState: setClickEducation,
-                clickedState: isClickedEducation,
-              })}
-              className="mb-6 text-2xl font-semibold hover:underline flex items-center"
-            >
-              <Image
-                src={isClickedEducation ? "/down.png" : "/right.png"}
-                alt="Toggle"
-                width={20}
-                height={20}
-                className="mr-2"
-              />
-              Education and Awards
-            </button>
-            {isClickedEducation && <Education />}
-          </div>
+          {/* CONNECT */}
+          <button
+            onClick={handleClick({
+              setClickedState: setClickConnect,
+              clickedState: isClickedConnect,
+            })}
+            className="mb-6 text-2xl font-semibold hover:underline flex items-center"
+          >
+            <Image
+              src={isClickedConnect ? "/down.png" : "/right.png"}
+              alt="Toggle"
+              width={20}
+              height={20}
+              className="mr-2"
+            />
+            Connect
+          </button>
+          {isClickedConnect && <Connect />}
+          {/* EDUCATION AND AWARDS */}
+          <button
+            onClick={handleClick({
+              setClickedState: setClickEducation,
+              clickedState: isClickedEducation,
+            })}
+            className="mb-6 text-2xl font-semibold hover:underline flex items-center"
+          >
+            <Image
+              src={isClickedEducation ? "/down.png" : "/right.png"}
+              alt="Toggle"
+              width={20}
+              height={20}
+              className="mr-2"
+            />
+            Education and Awards
+          </button>
+          {isClickedEducation && <Education />}
+          {/* WORK EXPERIENCE */}
+          <button
+            onClick={handleClick({
+              setClickedState: setClickWorkExperience,
+              clickedState: isClickedWorkExperience,
+            })}
+            className="mb-6 text-2xl font-semibold hover:underline flex items-center"
+          >
+            <Image
+              src={isClickedWorkExperience ? "/down.png" : "/right.png"}
+              alt="Toggle"
+              width={20}
+              height={20}
+              className="mr-2"
+            />
+            Work Experience
+          </button>
+          {isClickedWorkExperience && <WorkExperience />}{" "}
         </div>
       </div>
     </main>
