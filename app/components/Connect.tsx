45f957030f7a1f3693317d1ec043cafe3092a2e9
YASH KUMAR <kyash03@student.ubc.ca>
2023-12-25 02:09:34 -0800

    added mui icons to connect section

--- app/components/Connect.tsx
+++ app/components/Connect.tsx
@@ -1,29 +1,19 @@
-import Image from "next/image";
+import EmailIcon from "@mui/icons-material/Email";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 export default function Connect() {
   return (
     <div className="border-l-2 border-gray-200 pl-4 mb-6">
       <p className="p-1 flex items-center">
-        <Image
-          src="/envelope.png"
-          alt="email-icon"
-          width={20}
-          height={20}
-          className="mr-2"
-        />
+        <EmailIcon className="mr-2" />
         <span className="font-semibold mr-1">Email:</span>
         <a className="underline" href="mailto:kyash03@student.ubc.ca">
           kyash03@student.ubc.ca
         </a>
       </p>
       <p className="p-1 flex items-center">
-        <Image
-          src="/linkedin.png"
-          alt="LinkedIn icon"
-          width={20}
-          height={20}
-          className="mr-2"
-        />
+        <LinkedInIcon className="mr-2" />
         <span className="font-semibold mr-1">LinkedIn:</span>
         <a
           className="underline"
@@ -34,13 +24,7 @@ export default function Connect() {
         </a>
       </p>
       <p className="p-1 flex items-center">
-        <Image
-          src="/github.png"
-          alt="GitHub icon"
-          width={20}
-          height={20}
-          className="mr-2"
-        />
+        <GitHubIcon className="mr-2" />
         <span className="font-semibold mr-1">GitHub:</span>
         <a
           className="underline"
