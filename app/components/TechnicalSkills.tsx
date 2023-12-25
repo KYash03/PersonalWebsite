b450516a2555fe10d419c84c5fe6e8293846e483
YASH KUMAR <kyash03@student.ubc.ca>
2023-12-25 02:02:38 -0800

    added mui

--- app/components/TechnicalSkills.tsx
+++ app/components/TechnicalSkills.tsx
@@ -1,19 +1,19 @@
 export default function TechnicalSkills() {
   return (
     <div className="border-l-2 border-gray-200 pl-4 mb-6">
-      <p>
+      <p className="pb-1">
         <strong>Languages:</strong> Java, Python, R, JavaScript, TypeScript,
         SQL, C/C++, Swift.
       </p>
-      <p>
+      <p className="pb-1">
         <strong>Data Science:</strong> NumPy, Pandas, Matplotlib, Seaborn,
         Scikit-Learn, Tableau, Keras.
       </p>
-      <p>
+      <p className="pb-1">
         <strong>Web Development:</strong> HTML, CSS, Node.js, Express.js, React,
         jQuery, Tailwind CSS, Material UI, Mongoose, Bootstrap, Flask, JSP.
       </p>
-      <p>
+      <p className="pb-1">
         <strong>Cloud & Databases:</strong> PostgreSQL, MongoDB, GCP, AWS,
         Docker, Microsoft SQL Server.
       </p>
