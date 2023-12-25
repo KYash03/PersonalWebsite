1f44afc597c2e9f9365f1eb975c963e5a55acc77
YASH KUMAR <kyash03@student.ubc.ca>
2023-12-24 22:36:34 -0800

    reduced top margin of footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Connect() {
   const lastUpdatedDate = "December 24, 2023";
   return (
-    <div className="text-center py-3 border-t-2 border-black mt-10 w-full max-w-screen-md mx-auto">
+    <div className="text-center py-3 border-t-2 border-black mt-8 w-full max-w-screen-md mx-auto">
       <p>Last Updated: {lastUpdatedDate}</p>
     </div>
   );
