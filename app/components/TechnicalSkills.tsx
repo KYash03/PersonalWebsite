1288ce578b95607b392cb03f8749ba8ecd2219a7
YASH KUMAR <kyash03@student.ubc.ca>
2023-12-26 17:32:58 -0800

    modified skills section

--- app/components/TechnicalSkills.tsx
+++ app/components/TechnicalSkills.tsx
@@ -1,24 +1,24 @@
 export default function TechnicalSkills() {
   return (
     <div className="border-l-2 border-gray-200 pl-4 mb-6">
-      <p className="pb-1">
+      <p className="pb-0.5">
         <strong>Languages:</strong> Java, Python, R, JavaScript, TypeScript,
-        SQL, C/C++, Swift.
+        SQL, C/C++, Swift
       </p>
-      <p className="pb-1">
+      <p className="pb-0.5">
         <strong>Data Science:</strong> NumPy, Pandas, Matplotlib, Seaborn,
-        Scikit-Learn, Tableau, Keras.
+        Scikit-Learn, Tableau, Keras
       </p>
-      <p className="pb-1">
+      <p className="pb-0.5">
         <strong>Web Development:</strong> HTML, CSS, Node.js, Express.js, React,
         jQuery, Tailwind CSS, Material UI, Mongoose, Bootstrap, Flask, JSP.
       </p>
-      <p className="pb-1">
+      <p className="pb-0.5">
         <strong>Cloud & Databases:</strong> PostgreSQL, MongoDB, GCP, AWS,
-        Docker, Microsoft SQL Server.
+        Docker, Microsoft SQL Server
       </p>
       <p>
-        <strong>Frameworks:</strong> UIKit, SwiftUI, Streamlit, Core ML, JUnit.
+        <strong>Frameworks:</strong> UIKit, SwiftUI, Streamlit, Core ML, JUnit
       </p>
     </div>
   );
