14b54f1b0d693940047fc57d11bef22c6bafbf96
YASH KUMAR <kyash03@student.ubc.ca>
2023-12-26 19:55:32 -0800

    added intro

--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -1,13 +1,12 @@
 export default function Intro() {
   return (
-    <>
-      <p>Hi, I&apos;m Yash!</p>
-      <p>
-        I&apos;m a Data Science student at UBC with a strong machine learning,
-        data analysis, and web and mobile app development background. I&apos;m
-        seeking an internship position to apply and further develop skills in
-        dynamic and challenging environments.
+    <div>
+      <p className="mb-1">
+        Hi! I&apos;m Yash, a third-year Data Science student at UBC. I
+        specialize in machine learning, data analysis and developing web and
+        mobile apps.
       </p>
-    </>
+      <p>For a detailed look, check out my resume at the end of this page.</p>
+    </div>
   );
 }
