a71c20b5413dc477d3e16cfabd33b6a220b0f9ac
YASH KUMAR <kyash03@student.ubc.ca>
2023-12-30 21:51:02 -0800

    updated resume

--- app/components/Resume.tsx
+++ app/components/Resume.tsx
@@ -2,7 +2,7 @@ export default function Resume() {
   return (
     <div className="border-l-2 border-gray-200 pl-4 mb-6">
       <iframe
-        src="/Résumé - Yash Kumar.pdf"
+        src="/Yash Kumar.pdf"
         style={{ width: "100%", height: "500px" }}
       ></iframe>
     </div>
