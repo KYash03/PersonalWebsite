8457af28b666442b920262ebd07137875259bdb5
Yash Kumar <112984584+KYash03@users.noreply.github.com>
2024-01-10 18:24:19 -0800

    Update Intro.tsx

--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -4,7 +4,7 @@ export default function Intro() {
       <p className="mb-1">
         Hi! I&apos;m Yash, a third-year Data Science student at UBC. I specialize in developing web and mobile apps, data analysis and machine learning.
       </p>
-      <p>For a detailed look, check out my résumé at the end of this page.</p>
+      <p>On this page, you can learn how to connect with me, my education, accolades, work experience, projects and skills. Check out my résumé at the end of this page for a more detailed look.</p>
     </div>
   );
 }
