b61f42411271063f0bbbb4ea8e91bdf6ee074f62
YASH KUMAR <kyash03@student.ubc.ca>
2024-03-22 07:34:05 -0700

    need to update resume

--- app/components/Resume.tsx
+++ app/components/Resume.tsx
@@ -1,10 +1,11 @@
 export default function Resume() {
   return (
     <div className="border-l-2 border-gray-200 pl-4 mb-6">
-      <iframe
+      {/* <iframe
         src="/Yash Kumar.pdf"
         style={{ width: "100%", height: "500px" }}
-      ></iframe>
+      ></iframe> */}
+      To be updated...
     </div>
   );
 }
