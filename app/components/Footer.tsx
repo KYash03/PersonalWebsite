ba6e9d628bca80f25f12e42a14ea31bf6d734452
Yash Kumar <112984584+KYash03@users.noreply.github.com>
2024-03-29 23:33:03 -0700

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,5 +1,5 @@
 export default function Connect() {
-  const lastUpdatedDate = "March 22, 2024";
+  const lastUpdatedDate = "March 29, 2024";
   return (
     <div className="text-center py-3 border-t-2 border-black mt-8 w-full max-w-screen-md mx-auto">
       <p>Last Updated: {lastUpdatedDate}</p>
