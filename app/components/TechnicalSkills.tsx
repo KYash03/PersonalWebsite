093241a1c8a53c20837f20ceb70d94a61c44e4c0
Yash Kumar <112984584+KYash03@users.noreply.github.com>
2024-04-05 04:07:09 -0700

    Update TechnicalSkills.tsx

--- app/components/TechnicalSkills.tsx
+++ app/components/TechnicalSkills.tsx
@@ -2,23 +2,26 @@ export default function TechnicalSkills() {
   return (
     <div className="border-l-2 border-gray-200 pl-4 mb-6">
       <p className="pb-0.5">
-        <strong>Languages:</strong> Java, Python, R, JavaScript, TypeScript,
-        SQL, C/C++, Swift
+        <strong>Languages:</strong> Python, Java, C++, SQL, JavaScript,
+        TypeScript, Swift, C, R, MATLAB
       </p>
       <p className="pb-0.5">
-        <strong>Data Science:</strong> NumPy, Pandas, Matplotlib, Seaborn,
-        Scikit-Learn, Tableau, Keras
+        <strong>Web Development:</strong> React, Node.js, Express.js, Next.js,
+        Tailwind CSS, Bootstrap, Material UI, Flask, FastAPI, Axios, Mongoose,
+        jQuery, HTML, CSS, JSP
       </p>
       <p className="pb-0.5">
-        <strong>Web Development:</strong> HTML, CSS, Node.js, Express.js, React,
-        jQuery, Tailwind CSS, Material UI, Mongoose, Bootstrap, Flask, JSP
+        <strong>Data Science:</strong> TensorFlow, Keras, Pandas, NumPy,
+        Scikit-Learn, Tableau, Matplotlib, Seaborn, Prophet, Looker Studio,
+        Jupyter
       </p>
       <p className="pb-0.5">
-        <strong>Cloud & Databases:</strong> PostgreSQL, MongoDB, GCP, AWS,
-        Docker, Microsoft SQL Server
+        <strong>Frameworks & Libraries:</strong> React Native, SwiftUI,
+        Streamlit, UIKit, GeoPandas, Core ML, SwiftUI, JUnit
       </p>
       <p>
-        <strong>Frameworks:</strong> UIKit, SwiftUI, Streamlit, Core ML, JUnit
+        <strong>Tools, Database and Cloud:</strong> AWS, GCP, Docker, MongoDB,
+        PostgreSQL, GitHub, Microsoft SQL Server, Postman, Create ML
       </p>
     </div>
   );
