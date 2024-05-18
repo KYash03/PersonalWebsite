abbbbaa5eefa02c6fca843af257f7adb8b552639
YASH KUMAR <kyash03@student.ubc.ca>
2024-05-18 16:28:26 -0400

    made education section responsive

--- app/components/Education.tsx
+++ app/components/Education.tsx
@@ -24,14 +24,19 @@ export default function Education() {
           <EmojiEventsIcon className="mr-2" />
           <p className="font-semibold text-2xl">Dean&apos;s List</p>
         </div>
-        <div className="flex items-center mb-2">
-          <DateRangeIcon className="mr-2" />
-          <span className="font-semibold mr-4">June 2023 </span>
-          <BusinessIcon className="mr-2" />
-          <span className="font-semibold">
-            The University of British Columbia, Okanagan
-          </span>
+        <div className="flex flex-col md:flex-row md:items-center md:mb-2">
+          <div className="flex items-center mb-2 md:mb-0 md:mr-4">
+            <DateRangeIcon className="mr-2 md:mr-2" />
+            <span className="font-semibold mr-4 md:mr-0">June 2023</span>
+          </div>
+          <div className="flex items-center">
+            <BusinessIcon className="mr-2 md:mr-2" />
+            <span className="font-semibold">
+              The University of British Columbia, Okanagan
+            </span>
+          </div>
         </div>
+
         <p>
           The Dean&apos;s List recognizes students who have demonstrated
           academic excellence by maintaining a sessional average of at least
